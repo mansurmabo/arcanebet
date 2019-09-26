e7b6af339e48be96e908ca8a514078300493744c
Mansur Mabo <mansurmabo@gmail.com>
2019-09-27 03:58:01 +0600

    integrate with exchange rate api

---
+++ app/models/exchange_rate_api.rb
@@ -0,0 +1,24 @@
+class ExchangeRateApi
+  include HTTParty
+  attr_accessor :base, :start_date, :end_date
+
+  base_uri 'http://api.exchangeratesapi.io'
+
+  def initialize(options = {})
+    @options = {
+        query: {
+          base: options[:base],
+          start_at: options[:start_date],
+          end_at: options[:end_date],
+        }
+    }
+  end
+
+  def latest
+    self.class.get('/latest', @options)
+  end
+
+  def historical
+    self.class.get('/history', @options)
+  end
+end
